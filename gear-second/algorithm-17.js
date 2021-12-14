54791a0aaa04c2d330d2bb32bf628185743a6a19
曾瑞阳 <wb.zengruiyang@mesg.corp.netease.com>
2021-12-14 19:07:17 +0800

    change:17

--- gear-second/algorithm-17.js
+++ gear-second/algorithm-17.js
@@ -41,10 +41,69 @@ var isSub = function (A, B) {
 /**
  * 监控二叉树
  */
+var minCameraCover = function (root) {
+    var count = 0;
+    var dfs = function (root) {
+        if (!root) {
+            return 2;
+        }
+        var left = dfs(root.left);
+        var right = dfs(root.right);
+
+        if (left === 0 || right === 0) {
+            count++;
+            return 1;
+        }
+        if (left === 1 || right === 1) {
+            return 2;
+        }
+        if (left === 2 && right === 2) {
+            return 0;
+        }
+    };
+    if (dfs(root) === 0) {
+        count++;
+    }
+    return count;
+};
 
 /**
- *
+ * 二叉树最大宽度
  */
+var widthOfBinaryTree = function (root) {
+    let maxWidth = 0;
+    if (root) {
+        let queue = [root];
+        let xs = [1];
+        while (queue.length > 0) {
+            let size = queue.length;
+            let left, right;
+            if (size === 1) {
+                xs = [1];
+            }
+            for (let i = 0; i < size; i++) {
+                let node = queue.shift();
+                let x = xs.shift();
+                if (i === 0) {
+                    left = x;
+                }
+                if (i === size - 1) {
+                    right = x;
+                }
+                if (node.left) {
+                    queue.push(node.left);
+                    xs.push(x * 2);
+                }
+                if (node.right) {
+                    queue.push(node.right);
+                    xs.push(x * 2 + 1);
+                }
+            }
+            maxWidth = Math.max(maxWidth, right - left + 1);
+        }
+    }
+    return maxWidth;
+};
 
 /**
  * 二叉树的前序遍历
