bebfd5a22dafd96ad826c7d669bbb595c901a847
曾瑞阳 <wb.zengruiyang@mesg.corp.netease.com>
2022-01-04 21:58:16 +0800

    change:25

---
+++ gear-second/algorithm-25.js
@@ -0,0 +1,28 @@
+/**
+ * 用Rand7()实现Rand10()
+ */
+var rand10 = function () {
+    var a, b, idx;
+    while (true) {
+        a = rand7();
+        b = rand7();
+        idx = b + (a - 1) * 7;
+        if (idx <= 40) {
+            return 1 + ((idx - 1) % 10);
+        }
+        a = idx - 40;
+        b = rand7();
+        // get uniform dist from 1 - 63
+        idx = b + (a - 1) * 7;
+        if (idx <= 60) {
+            return 1 + ((idx - 1) % 10);
+        }
+        a = idx - 60;
+        b = rand7();
+        // get uniform dist from 1 - 21
+        idx = b + (a - 1) * 7;
+        if (idx <= 20) {
+            return 1 + ((idx - 1) % 10);
+        }
+    }
+};
