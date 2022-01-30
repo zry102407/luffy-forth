e349d76628f87761ddc91b14005d7a014b1fcca8
曾瑞阳 <wb.zengruiyang@mesg.corp.netease.com>
2022-01-30 23:41:20 +0800

    change: 27代码提交

--- gear-second/algorithm-27.js
+++ gear-second/algorithm-27.js
@@ -1,4 +1,104 @@
 /**
+ * 排序数组
+ */
+var sortArray = function (nums) {
+    mergeSort(nums, 0, nums.length - 1);
+    return nums;
+};
+
+function mergeSort(nums, left, right) {
+    if (left >= right) return;
+    const mid = Math.floor((right + left) / 2);
+    mergeSort(nums, left, mid);
+    mergeSort(nums, mid + 1, right);
+    const tmp = [];
+    let i = left,
+        j = mid + 1;
+    let count = 0;
+    while (i <= mid && j <= right) {
+        if (nums[i] <= nums[j]) {
+            tmp[count++] = nums[i++];
+        } else {
+            tmp[count++] = nums[j++];
+        }
+    }
+    while (i <= mid) {
+        tmp[count++] = nums[i++];
+    }
+    while (j <= right) {
+        tmp[count++] = nums[j++];
+    }
+    for (let k = 0; k < right - left + 1; ++k) {
+        nums[left + k] = tmp[k];
+    }
+}
+/**
+ * 数组中的逆序对
+ */
+const reversePairs = (nums) => {
+    // 记录逆序
+    let count = 0;
+    // 归并排序
+    const mergeSort = (arr) => {
+        // arr为一个值直接返回
+        if (arr.length < 2) return arr;
+        // 长度为2排序返回
+        if (arr.length === 2) {
+            if (arr[0] <= arr[1]) return arr;
+            // 记录逆序
+            count++;
+            return [arr[1], arr[0]];
+        }
+        // 获取二分位置
+        const mid = arr.length >> 1;
+        // 将左边部分排序
+        const left = mergeSort(arr.slice(0, mid));
+        // 将右边部分排序
+        const right = mergeSort(arr.slice(mid));
+        // 保存结果
+        const res = [];
+        // 两组的起始指针位置
+        let [leftIndex, rightIndex] = [0, 0];
+        // 循环直到两边遍历完
+        while (leftIndex < left.length || rightIndex < right.length) {
+            // left结束
+            if (leftIndex >= left.length) {
+                // 将剩余right添加到res
+                res.push.apply(res, right.slice(rightIndex));
+                // 跳出循环
+                break;
+            }
+            // right结束
+            if (rightIndex >= right.length) {
+                // 将剩余left添加到res
+                res.push.apply(res, left.slice(leftIndex));
+                // 跳出循环
+                break;
+            }
+            // 左值小
+            if (left[leftIndex] <= right[rightIndex]) {
+                // 将左值push
+                res.push(left[leftIndex]);
+                // 左下标右移
+                leftIndex++;
+            } else {
+                //右值小
+                // 将右值push
+                res.push(right[rightIndex]);
+                // 右下标右移
+                rightIndex++;
+                // 记录逆序(因为left是有序的，所以leftIndex开始都存在逆序)
+                count += left.length - leftIndex;
+            }
+        }
+        // 返回有序数组
+        return res;
+    };
+    mergeSort(nums);
+    return count;
+};
+
+/**
  * 排序链表
  */
 const merge = (head1, head2) => {
@@ -74,4 +174,25 @@ const merge = (arr1, arr2) => {
     while (idx1 < arr1.length) ret.push(arr1[idx1++]);
     while (idx2 < arr2.length) ret.push(arr2[idx2++]);
     return ret;
+};
+
+/**
+ * 首个共同祖先
+ */
+var lowestCommonAncestor = function (root, p, q) {
+    let ans;
+    const dfs = (root, p, q) => {
+        if (root === null) return false;
+        const lson = dfs(root.left, p, q);
+        const rson = dfs(root.right, p, q);
+        if (
+            (lson && rson) ||
+            ((root.val === p.val || root.val === q.val) && (lson || rson))
+        ) {
+            ans = root;
+        }
+        return lson || rson || root.val === p.val || root.val === q.val;
+    };
+    dfs(root, p, q);
+    return ans;
 };
